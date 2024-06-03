9951a86251c2b3424fd692b952c1585af64065a7
Wasif Ali Kazmi <88131946+wasif92642@users.noreply.github.com>
2024-06-03 09:46:16 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import { getStorage } from "@firebase/storage";
 import { getFirestore } from "@firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyDZYR_IuvxilpFwgZXZqtzTY6qA8auTyEU",
-  authDomain: "vita-319b7.firebaseapp.com",
-  databaseURL: "https://vita-319b7-default-rtdb.firebaseio.com",
-  projectId: "vita-319b7",
-  storageBucket: "vita-319b7.appspot.com",
-  messagingSenderId: "929998147668",
-  appId: "1:929998147668:web:f5fb7e9de11e56c4a05dc1",
-  measurementId: "G-H1N91K1G42",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(config);
